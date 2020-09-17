024e50fd42624eae9bd7d3bcfc86086132edd8ee
Ashish2000L <ashishkumar28april@gmail.com>
2020-09-17 22:38:25 +0530

    uploading to test pypi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import pathlib
 
 here = pathlib.Path(__file__).parent.resolve()
-long_description = (here / 'README.md').read_text(encoding='utf-8')
+long_description = (here / 'README.txt').read_text(encoding='utf-8')
 
 setup(
     name="linear_prog",
